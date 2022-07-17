a1bf9a939a857c49d2fc29526a3fb04bc273532d
Tiago <tiago.freitas.faria@gmail.com>
2022-07-17 18:32:09 -0300

    criação dos itens de menu para usuário logado

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,54 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { useAuth } from '../contexts/AuthContext'
+import { UsersController } from '../controllers/UsersController'
+import { User } from '../interfaces/User'
 import '../styles/sidebar.css'
 
 interface SidebarProps {
 	onClose: () => void
 }
 
-export function Sidebar({ onClose }: SidebarProps) {
+interface MenuLinkProps {
+	link: string
+	text: string
+	icon: string
+}
+
+export function Sidebar({ onClose: close }: SidebarProps) {
+	const { signed } = useAuth()
+
+	const [user, setUser] = useState<User>()
+
+	useEffect(() => {
+		if (signed)
+			new UsersController().show().then(data => setUser(data))
+		else
+			setUser(undefined)
+	}, [signed])
+
+	function CloseBtn() {
+		return (
+			<i
+				className='fa-solid fa-times fa-2x absolute top-8 right-8 cursor-pointer'
+				onClick={close}
+			/>
+		)
+	}
+
+	function MenuLink({ icon, link, text }: MenuLinkProps) {
+		return (
+			<Link
+				to={link}
+				className='border hover:bg-blue-500 p-2'
+				onClick={close}
+			>
+				<i className={`mr-1 fa-solid fa-${icon}`} /> {text}
+			</Link>
+		)
+	}
+
 	return (
 		<aside
 			className={
@@ -15,21 +57,40 @@ export function Sidebar({ onClose }: SidebarProps) {
 			}
 			id='sidebar'
 		>
-			<header>
-				<i
-					className='fa-solid fa-times fa-2x absolute top-8 right-8 cursor-pointer'
-					onClick={onClose}
-				/>
-				<h3 className='leading-7 text-2xl'>
-					Bem-vindx ao<br />MyCalendar
-				</h3>
+			<header className='flex flex-col justify-center text-center'>
+				<CloseBtn />
+
+				{ user ?
+					<>
+						<img
+							src={user.picture ?? ''}
+							alt={user.name}
+							className='p-1 bg-white border rounded-[50%] w-20 h-20 mx-auto mb-4'
+						/>
+
+						<strong>{user.name}</strong>
+					</> :
+					<h3 className='leading-7 text-2xl'>Bem-vindx ao<br />MyCalendar</h3>
+				}
 			</header>
 
-			<div>
-				<Link to='/login' className='bg-blue-500 hover:bg-blue-600'>Entrar</Link>
+			{ user ?
+				<div>
+					<MenuLink icon='calendar-plus' text='Cadastrar Evento' link='/create_event' />
+					<MenuLink icon='calendar-days' text='Próximos Eventos' link='/next_events' />
+					<MenuLink icon='key' text='Alterar Senha' link='/change_password' />
+					<MenuLink icon='arrow-right-from-bracket' text='Sair' link='/logout' />
+				</div> :
+				<div>
+					<Link to='/login' className='bg-blue-500 hover:bg-blue-600' onClick={close}>
+						Entrar
+					</Link>
 
-				<Link to='/register' className='bg-purple-500 hover:bg-purple-600'>Cadastre-se</Link>
-			</div>
+					<Link to='/register' className='bg-purple-500 hover:bg-purple-600' onClick={close}>
+						Cadastre-se
+					</Link>
+				</div>
+			}
 
 			<footer className='mb-3'>
 				<p>&copy; 2022 MyCalendar</p>
