40ecc59c7dcd941016148d674896f58938e5a87c
Tiago Freitas <tiago.freitas.faria@gmail.com>
2022-07-22 14:02:38 -0300

    ajuste nas propriedades e estilização das labels

--- src/components/Labels.tsx
+++ src/components/Labels.tsx
@@ -3,7 +3,7 @@ import { Label } from '../interfaces/Label'
 interface LabelsProps {
 	data: Label[]
 	showRemoveButton?: boolean
-	onRemove: (id: number) => void
+	onRemove?: (id: number) => void
 }
 
 export function Labels({ data, showRemoveButton, onRemove: remove }: LabelsProps) {
@@ -13,15 +13,15 @@ export function Labels({ data, showRemoveButton, onRemove: remove }: LabelsProps
 				return (
 					<span
 						key={label.id}
-						style={{ border: `1px solid ${label.color}` }}
-						className="pl-4 pr-3 py-2 rounded-lg"
+						style={{ border: `2px solid ${label.color}` }}
+						className="px-4 py-2 rounded-lg"
 					>
 						{label.name}
 
 						{ showRemoveButton &&
 							<span
 								className='cursor-pointer ml-2 mr-0 text-gray-300 text-lg'
-								onClick={() => remove(label.id)}
+								onClick={() => remove ? remove(label.id) : null}
 							>
 								&times;
 							</span>
